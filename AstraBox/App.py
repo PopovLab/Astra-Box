a4990353b2aa0059eaafffc7c34035d6a3a836c2
Alex <temper8@gmail.com>
2022-09-13 17:34:57 +0300

    create App

---
+++ AstraBox/App.py
@@ -0,0 +1,5 @@
+class App:
+    def __init__(self, root):
+        root.title("ASTRA Box")
+        root.minsize(800, 450)
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from AstraBox.App import App
+
+if __name__ == '__main__':
+    root = tk.Tk() 
+    m = App(root)
+    root.mainloop()
