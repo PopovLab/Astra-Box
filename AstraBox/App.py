f49a0537ccf79cf42915632f80557319e4515c94
Alex <temper8@gmail.com>
2022-09-14 16:27:37 +0300

    catch of  window close event

--- AstraBox/App.py
+++ AstraBox/App.py
@@ -1,5 +1,18 @@
+import tkinter 
+import tkinter.messagebox as messagebox
+
 class App:
     def __init__(self, root):
         root.title("ASTRA Box")
         root.minsize(800, 450)
 
+        root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.root = root
+
+
+    def on_closing(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            #self.controller.destroy()
+            #Storage().close()
+            self.root.destroy()
+            
