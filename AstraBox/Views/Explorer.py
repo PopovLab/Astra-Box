deda5a7cae5014f0f2c098b51afae4ca9683273c
Alex <temper8@gmail.com>
2022-09-25 22:15:08 +0300

    add select item and selection_clear

--- AstraBox/Views/Explorer.py
+++ AstraBox/Views/Explorer.py
@@ -6,7 +6,6 @@ class Explorer(ttk.Frame):
         super().__init__(master)      
         self.on_select = None
         self.model_store = model_store
-        #self.storage = store
         lab = ttk.Label(self, text=title)
         lab.grid(row=0, column=0, sticky=tk.W)
         self.nodes = {}
@@ -25,10 +24,15 @@ class Explorer(ttk.Frame):
         self.tree.grid(row=1, column=0,  columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
         ysb.grid(row=1, column=2, sticky=tk.N + tk.S)
         #xsb.grid(row=2, column=0, sticky=tk.E + tk.W)
-        #self.tree.bind("<<TreeviewSelect>>", self.select_node)
+        self.tree.bind("<<TreeviewSelect>>", self.select_node)
         self.rowconfigure(0, weight=0)
         self.rowconfigure(1, weight=1)
         self.columnconfigure(1, weight=1)        
+
+
+    def selection_clear(self):
+        print('explorer selection clear')
+        self.tree.selection_set(())
 
     def update_tree(self):
         if self.model_store:
@@ -42,4 +46,16 @@ class Explorer(ttk.Frame):
             model = self.model_store.data[key]
             status = 'ok'
             #self.nodes[uuid] = 
-            self.tree.insert('', tk.END, text=key, values=(status,),tags=('model',))  
+            self.tree.insert('', tk.END, text=key, values=(status,), tags=('model',))  
+
+    def select_node(self, event):
+        print('Explorer select_node ')
+        sel_id = self.tree.selection()
+        print(f"selection = {sel_id}")
+        if len(sel_id)>0:
+            selected_item = self.tree.item(sel_id)
+            tag = selected_item["tags"][0]            
+            print(selected_item)
+            print(tag)
+            if self.on_select:
+                self.on_select(self)
--- AstraBox/Views/RackFrame.py
+++ AstraBox/Views/RackFrame.py
@@ -7,7 +7,7 @@ class RackFrame(ttk.Frame):
     def __init__(self, master) -> None:
         super().__init__(master)
         self.on_select = None
-
+        self.active_exlorer = None
         self.v = tk.StringVar(self, "xxx")  # initialize
 
         ttk.Separator(self, orient='horizontal').pack(fill='x')
@@ -18,23 +18,23 @@ class RackFrame(ttk.Frame):
         ttk.Separator(self, orient='horizontal').pack(fill='x')
 
         self.exp_explorer = Explorer(self, title='Experiments', model_store=Storage().exp_store)
-        #self.spectrum_explorer.on_select = self.explorer_select
+        self.exp_explorer.on_select = self.on_explorer_select
         self.exp_explorer.pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,20))   
              
         self.exp_explorer = Explorer(self, title='Equlibrium', model_store=Storage().equ_store)
-        #self.spectrum_explorer.on_select = self.explorer_select
+        self.exp_explorer.on_select = self.on_explorer_select
         self.exp_explorer.pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,20))                
 
         ttk.Separator(self, orient='horizontal').pack(fill='x')
 
         self.exp_explorer = Explorer(self, title='Subroutine', model_store=Storage().sbr_store)
-        #self.spectrum_explorer.on_select = self.explorer_select
+        self.exp_explorer.on_select = self.on_explorer_select
         self.exp_explorer.pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,20))                
 
         ttk.Separator(self, orient='horizontal').pack(fill='x')
 
         self.exp_explorer = Explorer(self, title='Ray Tracing Configurations')
-        #self.spectrum_explorer.on_select = self.explorer_select
+        self.exp_explorer.on_select = self.on_explorer_select
         self.exp_explorer.pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,20))                
 
         ttk.Separator(self, orient='horizontal').pack(fill='x')
@@ -43,3 +43,10 @@ class RackFrame(ttk.Frame):
                             style = 'Toolbutton').pack(expand=0, fill=tk.X)
 
         ttk.Separator(self, orient='horizontal').pack(fill='x')
+
+    def on_explorer_select(self, explorer):
+        if self.active_exlorer:
+            if self.active_exlorer is not explorer:
+                self.active_exlorer.selection_clear()
+
+        self.active_exlorer = explorer
