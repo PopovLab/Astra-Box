101ddb4efaa324d997cc80c2f887a30edc6b2893
Alex <temper8@gmail.com>
2022-09-29 15:40:46 +0300

    add Physical parameters

--- AstraBox/Models/RTModel.py
+++ AstraBox/Models/RTModel.py
@@ -10,6 +10,63 @@ def default_rt_setting():
                     'title' : 'Comments',
                     'type' : 'text'
                 },
+            'Physical parameters': {
+                "Freq": { 
+                    'title' : 'Frequency',
+                    'value' : 5.0, 
+                    'unit'  : 'GHz',
+                    'type': 'float',                    
+                    'description' : "RF frequency, GHz"
+                },
+                "xmi1": { 
+                    'title' : 'xmi1',
+                    'value' : 2.0, 
+                    'type': 'float',
+                    'description' : "Mi1/Mp,  relative mass of ions 1"
+                },                
+               "zi1": { 
+                    'title' : 'zi1',
+                    'value' : 1.0, 
+                    'type': 'float',
+                    'description' : "charge of ions 1"
+                },                
+               "xmi2": { 
+                    'title' : 'xmi2',
+                    'value' : 16.0, 
+                    'type': 'float',
+                    'description' : "Mi2/Mp,  relative mass of ions 2"
+                },                        
+               "zi2": { 
+                    'title' : 'zi2',
+                    'value' : 0.0, 
+                    'type': 'float',
+                    'description' : "charge of ions 2"
+                },
+               "dni2": { 
+                    'title' : 'dni2',
+                    'value' : 0.03, 
+                    'type': 'float',
+                    'description' : "Ni2/Ni1, relative density of ions 2"
+                },
+               "xmi3": { 
+                    'title' : 'xmi3',
+                    'value' : 1.00, 
+                    'type': 'float',
+                    'description' : "Mi3/Mp,  relative mass of ions 3"
+                },
+               "zi3": { 
+                    'title' : 'zi3',
+                    'value' : 1.00, 
+                    'type': 'float',
+                    'description' : "charge of ions 3"
+                },
+               "dni3": { 
+                    'title' : 'dni3',
+                    'value' : 1.00, 
+                    'type': 'float',
+                    'description' : "Ni3/Ni1, relative density of ions 3"
+                },
+            },
             "Options" :{
                 'Xwkb/Np':{
                     'value': 'Automatic',
