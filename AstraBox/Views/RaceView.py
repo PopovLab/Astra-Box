4e887ac0669a32a1006e99aab276ffef7fdf56dc
Alex <temper8@gmail.com>
2022-10-12 17:23:08 +0300

    Update RaceView.py

--- AstraBox/Views/RaceView.py
+++ AstraBox/Views/RaceView.py
@@ -28,55 +28,82 @@ class RaceView(ttk.Frame):
 
         self.radial_data_list = model.get_radial_data_list()
 
-        self.var = tk.IntVar(name = 'index')
-        self.var.trace_add('write', lambda var, indx, mode: self.update_var())
+        self.index_var = tk.IntVar(name = 'index', value=1)
+        self.index_var.trace_add('write', lambda var, indx, mode: self.update_var())
 
-        slider = tk.Scale( self, variable = self.var, orient = tk.HORIZONTAL, from_=0, to=len(self.radial_data_list), resolution=1, length = 250 )
-        slider.grid(row=2, column=0, padx=5, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)       
-        self.time_var = tk.StringVar() 
-        self.label = tk.Label(master=self, textvariable = self.time_var)
-        self.label.grid(row=3, column=0, padx=5, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)        
-        self.plot = None
+        self.slider = tk.Scale( self, variable = self.index_var, orient = tk.HORIZONTAL, from_=1, to=len(self.radial_data_list), resolution=1, length = 250 )
+        self.slider.grid(row=2, column=0, padx=5, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)       
+        #self.time_var = tk.StringVar(value='xxx') 
+        #self.label = tk.Label(master=self, textvariable = self.time_var)
+        #self.label.grid(row=3, column=0, padx=5, pady=5,sticky=tk.N + tk.S + tk.E + tk.W)        
+        
+        profiles = self.get_profiles(1)
+        self.plot = SimplePlot(self, profiles)
+        self.plot.grid(row=4, column=0, sticky=tk.W, pady=4, padx=8)
+
+
+    def get_profiles(self, index):
+        file = self.radial_data_list[index]
+        print(file)
+        return self.model.read_radial_data(file)
 
     def update_var(self):
-        print(self.var.get())	        
-        file = self.radial_data_list[self.var.get()]
-        print(file)
-        profiles = self.model.read_radial_data(file)
-        txt = f'Astra radial data Time={profiles["Time"]}'
-        self.time_var.set(txt)
-        if self.plot:
-            self.plot.destroy()
-        self.plot = SimplePlot(self, profiles)
-        self.plot.grid(row=4, column=0, sticky=tk.W, pady=4, padx=8)           
+        profiles = self.get_profiles(self.index_var.get())
+        self.plot.update(profiles)
+        
 
 
 class SimplePlot(ttk.Frame):
     def __init__(self, master, profiles) -> None:
         super().__init__(master)  
-        self.fig, axs = plt.subplots(2, 2, figsize=(7, 6))
-        axs[0,0].plot(profiles['a'])
+        #self.fig, self.axs = plt.subplots(2, 2, figsize=(7, 6))
+        self.fig = plt.figure(figsize=(7, 6))
+        self.fig.suptitle(f'Astra radial data. Time={profiles["Time"]}')
+        self.axs = self.fig.subplots(2, 2)
+        self.profile_a, = self.axs[0,0].plot(profiles['a'])
         #axs[0,0].plot(profiles2['a'])    
-        axs[0,0].set_title("a")
+        self.axs[0,0].set_title("a")
 
-        axs[1,0].plot(profiles['J'])
+        self.profile_J, = self.axs[1,0].plot(profiles['J'])
         #axs[1,0].plot(profiles2['J'])
-        axs[1,0].set_title("J")
+        self.axs[1,0].set_title("J")
 
-        axs[0,1].plot(profiles['E'])
-        axs[0,1].set_title("E")
+        self.profile_E, = self.axs[0,1].plot(profiles['E'])
+        self.axs[0,1].set_title("E")
     
-        axs[1,1].plot(profiles['Johm'])
-        axs[1,1].set_title("Johm")
+        self.profile_Johm, = self.axs[1,1].plot(profiles['Johm'])
+        self.axs[1,1].set_title("Johm")
 
-        canvas = FigureCanvasTkAgg(self.fig, self)
-        canvas.draw()
-        canvas.get_tk_widget().grid(row=1, column=0)
+        self.canvas = FigureCanvasTkAgg(self.fig, self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=1, column=0)
         frame = ttk.Frame(self)
         frame.grid(row=0, column=0, sticky=tk.W)
-        toobar = NavigationToolbar2Tk(canvas, frame)
+        toobar = NavigationToolbar2Tk(self.canvas, frame)
         #tb = VerticalNavigationToolbar2Tk(canvas, frame)
         #canvas.get_tk_widget().grid(row=2, column=0)
+
+    def update(self, profiles):
+        self.fig.suptitle(f'Astra radial data. Time={profiles["Time"]}')
+
+        #self.profile_a.set_xdata([1,2,3,5])
+        self.profile_a.set_ydata(profiles['a']) 
+        self.axs[0,0].relim()
+        self.axs[0,0].autoscale_view(True,True,True)        
+        
+        self.profile_J.set_ydata(profiles['J']) 
+        self.axs[1,0].relim()
+        self.axs[1,0].autoscale_view(True,True,True)   
+
+        self.profile_E.set_ydata(profiles['E']) 
+        self.axs[0,1].relim()
+        self.axs[0,1].autoscale_view(True,True,True) 
+
+        self.profile_Johm.set_ydata(profiles['Johm']) 
+        self.axs[1,1].relim()
+        self.axs[1,1].autoscale_view(True,True,True)  
+
+        self.canvas.draw()
 
     def destroy(self):
         print("SimplePlot destroy")
