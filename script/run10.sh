ee11d35ad8546c3c4cfbbe3ed091620e07a798a9
Alex <temper8@gmail.com>
2022-10-31 22:35:24 +0300

    clear distribution folder

--- script/run10.sh
+++ script/run10.sh
@@ -3,6 +3,7 @@ source ~/intel/oneapi/setvars.sh
 echo 'clear folder:' $1/dat/
 rm $1/dat/* 
 rm $1/lhcd/out/*
+rm $1/lhcd/distribution/*
 echo 'run astra' $1 $2 $3
 $1/.exe/astra  $2 $3 
 read -p "Press enter to continue"
--- script/run11.sh
+++ script/run11.sh
@@ -10,6 +10,7 @@ source $INTEL
 echo 'clear folder:' $1/dat/
 rm $1/dat/* 
 rm $1/lhcd/out/*
+rm $1/lhcd/distribution/*
 echo 'run astra' $1 $2 $3
 $1/.exe/astra  $2 $3 
 read -p "Press enter to continue"
