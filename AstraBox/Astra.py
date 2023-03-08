303cb402d7f91a281a7e974bfe54de2959da131e
Alex <temper8@gmail.com>
2023-03-08 13:44:01 +0300

    Create Astra.py

---
+++ AstraBox/Astra.py
@@ -0,0 +1,15 @@
+RADIAL_DATA_PATH = "dat/XData"
+DIFFUSION_DATA_PATH = 'lhcd/diffusion/'
+MAXWELL_DATA_PATH = 'lhcd/maxwell/'
+DISTRIBUTION_PATH = 'lhcd/distribution/'
+TRAJECTROY_PATH = 'lhcd/out/traj'
+RT_RESULT_PATH =  'lhcd/rt-result'
+
+data_folder = {
+    'RADIAL_DATA'     : "dat/XData",
+    'DIFFUSION_DATA'  : 'lhcd/diffusion/',
+    'MAXWELL_DATA'    : 'lhcd/maxwell/',
+    'DISTRIBUTION'    : 'lhcd/distribution/',
+    'TRAJECTROY_PATH' : 'lhcd/out/traj',
+    'RT_RESULT_PATH'  : 'lhcd/rt-result'
+}
