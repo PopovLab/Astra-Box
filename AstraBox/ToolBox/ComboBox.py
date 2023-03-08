55ef7211374ef5107b4ae1b00f8be503b9f779cc
Alex <temper8@gmail.com>
2023-03-09 02:11:12 +0300

    create ConfigPanel

--- AstraBox/ToolBox/ComboBox.py
+++ AstraBox/ToolBox/ComboBox.py
@@ -14,12 +14,12 @@ class ComboBox(ttk.Frame):
     def get(self):
         return self.combo.get()
 
-    def __init__(self, master, title, values) -> None:
+    def __init__(self, master, title, values, width= 17) -> None:
         super().__init__(master)     
         self.selected_value = None
         label = ttk.Label(self, text=title)
         label.grid(row=0, column=0, sticky=tk.W, pady=4, padx=4)
-        self.combo = ttk.Combobox(self, width=17 )# command=lambda x=self: self.update(x))  
+        self.combo = ttk.Combobox(self, width= width )# command=lambda x=self: self.update(x))  
         self.combo.bind("<<ComboboxSelected>>", self.combo_selected)
         self.combo['values'] =  values  #item['value_items'] #
         #self.combo.set(item['value'])
--- AstraBox/Views/RunAstraView.py
+++ AstraBox/Views/RunAstraView.py
@@ -13,42 +13,22 @@ import AstraBox.Config as Config
 import AstraBox.WorkSpace as WorkSpace
 from AstraBox.ToolBox.ComboBox import ComboBox
 
-class RunAstraView(ttk.Frame):
+class ConfigPanel(ttk.Frame):
     def __init__(self, master) -> None:
         super().__init__(master)        
-        self.header_content =  { "title": "Run ASTRA", "buttons":[('Run calculation', self.start), ('Terminate', self.terminate)]}
-        self.astra_profiles = AstraBox.Models.AstraProfiles.default()
-        self.hp = HeaderPanel(self, self.header_content)
-        self.hp.grid(row=0, column=0, columnspan=5, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
-        self.columnconfigure(4, weight=1)        
-        #self.rowconfigure(0, weight=1)    
-
-        self.race_name = {'title': 'Race name', 'value': datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}
-        self.rn_wdg = StringBox(self, self.race_name, width=30)
-        self.rn_wdg.grid(row=1, column=0, columnspan=2, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
-
-        self.exp_combo = ComboBox(self, 'Exp', WorkSpace.getDataSource('exp').get_keys_list())
-        self.exp_combo.grid(row=2, column=0, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
-        self.equ_combo = ComboBox(self, 'Equ', WorkSpace.getDataSource('equ').get_keys_list())
-        self.equ_combo.grid(row=2, column=1, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)        
-        self.rt_combo = ComboBox(self, 'Ray tracing', WorkSpace.getDataSource('ray_tracing').get_keys_list())
-        self.rt_combo.grid(row=2, column=2, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
-        self.astra_combo = ComboBox(self, 'Astra profiles', Config.get_astra_profile_list())
-        self.astra_combo.grid(row=2, column=3, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
-
-        self.restore_last_run()
-
-        runframe = ttk.LabelFrame(self,  text=f"Calculation log:")
-        self.log_console = LogConsole(runframe)
-        self.log_console.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
-        runframe.columnconfigure(0, weight=1)
-        runframe.rowconfigure(1, weight=1)
-        self.first_init = True
-        #self.bind('<Visibility>', self.visibilityChanged)
-        runframe.grid(row=3, column=0, columnspan=5,  sticky=tk.N + tk.S + tk.E + tk.W)
-        self.rowconfigure(3, weight=1)
-
-    def restore_last_run(self):
+        self.exp_combo = ComboBox(self, 'Exp:', WorkSpace.getDataSource('exp').get_keys_list(), width= 20)
+        self.exp_combo.grid(row=0, column=0, padx=2, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.equ_combo = ComboBox(self, 'Equ:', WorkSpace.getDataSource('equ').get_keys_list())
+        self.equ_combo.grid(row=0, column=1, padx=2, sticky=tk.N + tk.S + tk.E + tk.W)        
+        self.rt_combo = ComboBox(self, 'Ray tracing:', WorkSpace.getDataSource('ray_tracing').get_keys_list(), width= 15)
+        self.rt_combo.grid(row=0, column=2, padx=2, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.astra_combo = ComboBox(self, 'Astra profiles:', Config.get_astra_profile_list(), width= 15)
+        self.astra_combo.grid(row=0, column=3, padx=2, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.columnconfigure(0, weight=1)  
+        self.columnconfigure(1, weight=1) 
+        self.columnconfigure(2, weight=1) 
+        self.columnconfigure(3, weight=1)         
+      
         ds = WorkSpace.getDataSource('races')
         p = ds.destpath.joinpath('last_run')
         if p.exists():
@@ -59,11 +39,40 @@ class RunAstraView(ttk.Frame):
             self.rt_combo.set(last_run['rt'])
             self.astra_combo.set(last_run['astra_profile'])          
 
+class RunAstraView(ttk.Frame):
+    def __init__(self, master) -> None:
+        super().__init__(master)        
+        self.header_content =  { "title": "Run ASTRA", "buttons":[('Run calculation', self.start), ('Terminate', self.terminate)]}
+        self.astra_profiles = AstraBox.Models.AstraProfiles.default()
+        self.hp = HeaderPanel(self, self.header_content)
+        self.hp.grid(row=0, column=0,  padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.columnconfigure(0, weight=1)        
+        #self.rowconfigure(0, weight=1)    
+
+        self.race_name = {'title': 'Race name', 'value': datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")}
+        self.rn_wdg = StringBox(self, self.race_name, width=40)
+        self.rn_wdg.grid(row=1, column=0, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
+
+        self.config_panel = ConfigPanel(self)
+        self.config_panel.grid(row=2, column=0, padx=5, sticky=tk.N + tk.S + tk.E + tk.W)
+
+        runframe = ttk.LabelFrame(self,  text=f"Calculation log:")
+        self.log_console = LogConsole(runframe)
+        self.log_console.grid(row=1, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+        runframe.columnconfigure(0, weight=1)
+        runframe.rowconfigure(1, weight=1)
+        self.first_init = True
+        #self.bind('<Visibility>', self.visibilityChanged)
+        runframe.grid(row=3, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.rowconfigure(3, weight=1)
+
+
+
     def start(self):
-        exp = self.exp_combo.get()
-        equ = self.equ_combo.get()
-        rt = self.rt_combo.get()
-        ap = self.astra_combo.get()
+        exp = self.config_panel.exp_combo.get()
+        equ = self.config_panel.equ_combo.get()
+        rt = self.config_panel.rt_combo.get()
+        ap = self.config_panel.astra_combo.get()
         
         race_model = RunModel(name= self.race_name['value'], exp_name= exp, equ_name= equ, rt_name= rt ) 
         
