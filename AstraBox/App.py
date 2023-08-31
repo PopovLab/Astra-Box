e3a4b0cb246f83ec21feb66197ac566273c7e5fe
Alex <temper8@gmail.com>
2023-08-31 15:52:22 +0300

    move TextPage to Pages folder

--- AstraBox/App.py
+++ AstraBox/App.py
@@ -9,7 +9,7 @@ from AstraBox.Views.ContentFrame import ContentFrame
 from AstraBox.Views.EmptyView import EmptyView
 from AstraBox.Views.RTModelView import RTModelView
 from AstraBox.Views.TextView import TextView
-from AstraBox.Views.TextPage import TextPage
+from AstraBox.Pages.TextPage import TextPage
 from AstraBox.Views.RaceView import RaceView
 from AstraBox.Views.RunAstraView import RunAstraView
 
