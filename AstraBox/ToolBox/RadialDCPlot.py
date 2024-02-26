6983c45a37579822a5f1b724ceda1f978e4e3609
Alex <temper8@gmail.com>
2024-02-26 14:55:21 +0300

    Update RadialDCPlot.py

--- AstraBox/ToolBox/RadialDCPlot.py
+++ AstraBox/ToolBox/RadialDCPlot.py
@@ -11,26 +11,6 @@ from AstraBox.ToolBox.VerticalNavigationToolbar import VerticalNavigationToolbar
 from AstraBox.Dialogs.PlotSettingDialog import PlotSettingDialog
 import AstraBox.ToolBox.ImageButton as ImageButton
 
-def default_radial_setting():
-    # настройки по умолчанию для рейтрейсинга
-    # профили токов
-    # профили E
-    # профили мощности
-    # профили температуры  
-    return {
-        'shape' : '2x2',
-        'title' : 'Radial Data setting',
-        'show grid': True,
-        'x axis' : 'rho',
-        'x axis terms' : ['index', 'ameter', 'rho'],
-        'plots'  : {
-            'ax1' : ['J', 'Johm', 'Jlh'],
-            'ax2' : ['E', 'En'],
-            'ax3' : ['Plh', 'Poh'],
-            'ax4' : ['Te']
-            }
-        }
-
 
 class RadialDCPlot(ttk.Frame):
     def __init__(self, master, data) -> None:
