f53f863f30559f6e3795b43d6bc804680e8f9c0b
Alex <temper8@gmail.com>
2024-08-26 11:51:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,5 @@ if __name__ == '__main__':
     App.work_space = History.get_last()
     
     while App.live:
-        print('----')
         App.run()
-        #app = App(ws)
-        #app.mainloop()
-    
-    #while Window.new_window:
-    #    w = Window.Window()
-     #   w.mainloop()
+
