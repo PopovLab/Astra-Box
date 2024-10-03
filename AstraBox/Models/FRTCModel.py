2ccf3ec5ede5e859bc2f10f57ae17eba1d934a79
Alex <temper8@gmail.com>
2024-10-03 17:04:14 +0300

    add Upl_fix and value

--- AstraBox/Models/FRTCModel.py
+++ AstraBox/Models/FRTCModel.py
@@ -86,8 +86,8 @@ class Options(ParametersSection):
     inew: int = Field(default= 0, title= 'inew', description= "inew=0 for usual tokamak&Ntor_grill; 1 or 2 for g' in ST&Npol_grill")                   
     itor: int = Field(default= 1, title= 'itor', description= "+-1, Btor direction in right coord{drho,dteta,dfi}")    
     ipol: int = Field(default= 1, title= 'ipol', description= "+-1, Bpol direction in right coord{drho,dteta,dfi}")    
-    #demo1: int = Field(default= 1, title= 'demo1', description= "demo1 option")    
-    #demo2: int = Field(default= 1, title= 'demo2', description= "demo2 option")   
+    upl_fix:   bool  = Field(default= False, title= 'Upl fixed', description= "enable fix Upl")    
+    upl_value: float = Field(default= 0.0, title= 'Upl value', description= "Upl value")   
 
 class GrillParameters(ParametersSection):
     name: ClassVar[str] = 'grill_parameters'
