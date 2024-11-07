049d9bd2bf5ddea18dba629d5464283ba13e1965
Alex <temper8@gmail.com>
2024-11-07 14:04:39 +0300

    refactoring RackFrame

--- AstraBox/Views/RackFrame.py
+++ AstraBox/Views/RackFrame.py
@@ -6,22 +6,7 @@ from AstraBox.Views.TableView import TableView
 
 
 def construct(master, app):
-    rf = RackFrame(master, app)
-    for f in app.work_space.folders:
-        if f.tag == 'top':
-            ListView(rf, f, command= rf.on_select_item).pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,5))
-
-    ttk.Separator(rf, orient='horizontal').pack(fill='x')
-
-    ttk.Radiobutton(rf, text="Run ASTRA", variable= rf.v, value="imped", width=25, 
-                    command= rf.show_RunAstraPage,
-                    style = 'Toolbutton').pack(expand=0, fill=tk.X)
-
-    ttk.Separator(rf, orient='horizontal').pack(fill='x')
-    for f in app.work_space.folders:
-        if f.tag == 'bottom':
-            TableView(rf, f, height= 8, command= rf.on_select_item).pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,10))
-    return rf
+    return RackFrame(master, app)
 
 class RackFrame(ttk.Frame):
     def __init__(self, master, app) -> None:
@@ -30,6 +15,24 @@ class RackFrame(ttk.Frame):
         self.on_select = None
         self.active_view = None
         self.v = tk.StringVar(self, "xxx")  # initialize
+        self.construct_ui()
+
+    def construct_ui(self):
+        for f in self.app.work_space.folders:
+            if f.tag == 'top':
+                ListView(self, f, command= self.on_select_item).pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,5))
+
+        ttk.Separator(self, orient='horizontal').pack(fill='x')
+
+        ttk.Radiobutton(self, text="Run ASTRA", variable= self.v, value="imped", width=25, 
+                        command= self.show_RunAstraPage,
+                        style = 'Toolbutton').pack(expand=0, fill=tk.X)
+
+        ttk.Separator(self, orient='horizontal').pack(fill='x')
+        for f in self.app.work_space.folders:
+            if f.tag == 'bottom':
+                TableView(self, f, height= 8, command= self.on_select_item).pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,10))        
+
 
     def on_select_item(self, sender, action):
         self.v.set('xxx')
